458298fefe9ddafd0c1f760e41811d4aaee5380c
adriansanchezsimon <alumno.506973@ies-azarquiel.es>
2022-05-30 15:52:02 +0200

    SUBIMOS EL PROYECTO
    A lo mejor esto el argentino tendria que haberlo explicado al principio. Para poder ir subiendo todos los cambios que ibamos haciendo y poder ver de verdad como funciona GITHUB

---
+++ src/main/java/com/cursojava/curso/models/Usuario.java
@@ -0,0 +1,32 @@
+package com.cursojava.curso.models;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "usuarios")
+@ToString @EqualsAndHashCode
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter @Setter @Column(name = "id")
+    Long id;
+    @Getter @Setter @Column(name = "nombre")
+    private String nombre;
+    @Getter @Setter @Column(name = "apellidos")
+    private String apellido;
+    @Getter @Setter @Column(name = "email")
+    private String email;
+    @Getter @Setter @Column(name = "telefono")
+    private String telefono;
+    @Getter @Setter @Column(name = "password")
+    private String password;
+
+
+
+}
